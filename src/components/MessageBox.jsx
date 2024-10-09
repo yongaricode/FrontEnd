56eb48159c1e253873d7b0d0221363064949024c
yongaricode <kang061313@gmail.com>
2024-10-09 17:34:29 +0900

    [Feat] json 객체 데이터 생성 및 가져오기

--- src/components/MessageBox.jsx
+++ src/components/MessageBox.jsx
@@ -2,9 +2,7 @@
 import Image from "next/image";
 import ribbon from "../../public/images/ribbon.png";
 
-const MessageBox = () => {
-  const userName = "용가리";
-
+const MessageBox = ({ user, content }) => {
   return (
     <div className="relative w-[300px] h-[200px] border-4 border-strong bg-white rounded-[25px]">
       <Image
@@ -13,8 +11,8 @@ const MessageBox = () => {
         alt="jingle-bell"
       />
       <div className="p-5 pt-[50px]  ">
-        <h3 className="font-semibold text-[20px]">From. {userName}</h3>
-        <p className="text-[17px]">좋은 하루 보내시길~</p>
+        <h3 className="font-semibold text-[20px]">From. {user}</h3>
+        <p className="text-[17px]">{content}</p>
       </div>
     </div>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,29 +2,50 @@
 // import Image from "next/image"
 
 import MessageBox from "@/components/MessageBox";
+import { useEffect, useState } from "react";
+import Image from "next/image";
 
 // 아래의 import문으로 MessageBox 컴포넌트를 사용할 수 있습니다.
 //import MessageBox from "@/components/MessageBox";
 
 export default function Home() {
+  const [messages, setMessages] = useState([]);
+
+  async function getMessageData() {
+    try {
+      const response = await fetch("/data/Message.json");
+      const data = await response.json();
+      setMessages(data.Message);
+      console.log(messages[0]);
+    } catch (error) {
+      console.error("메세지를 가져오지 못했습니다.");
+    }
+  }
+
+  useEffect(() => {
+    getMessageData();
+  }, []);
+
   return (
     <main className="flex flex-col w-screen h-screen bg-back items-center">
       <h1 className="text-[70px] font-['TTLaundryGothicB'] text-black pt-[60px]">
         연말 편지 우체통
       </h1>
       <h3 className="mt-[30px] font-bold text-[20px] font-['TTLaundryGothicB']">
-        💌 강다형님의 연말 편지 우체통으로 00건의 편지가 도착했어요!
+        💌 강다형님의 연말 편지 우체통으로 {messages.length}건의 편지가
+        도착했어요!
       </h3>
       <button className="px-[110px] py-[8px] mt-[25px] text-[20px] bg-strong rounded-[5px] font-semibold text-white font-['TTLaundryGothicB']">
         편지 쓰러 가기
       </button>
       <div className="grid grid-cols-4 gap-8 mt-[60px]">
-        <MessageBox />
-        <MessageBox />
-        <MessageBox />
-        <MessageBox />
-        <MessageBox />
-        <MessageBox />
+        {messages.map((message, index) => (
+          <MessageBox
+            key={index}
+            user={message.user}
+            content={message.content}
+          />
+        ))}
       </div>
     </main>
   );
